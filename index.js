5c7e67933a5c0fcc9274c84ab69660f98f99e3c9
todor-grigorov <t.grigorov99@gmail.com>
2023-08-30 23:31:41 +0200

    feat: connect BE to mongodb container

--- index.js
+++ index.js
@@ -13,6 +13,48 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 //#endregion
 
+//#region Mongoose
+// mongoose.set('strictQuery', true);
+main().catch(err => console.log(err));
+
+async function main() {
+    const options = {
+        useNewUrlParser: true,
+        autoIndex: false, // Don't build indexes
+          maxPoolSize: 10, // Maintain up to 10 socket connections
+      };
+
+      const connectWithRetry = () => {
+        mongoose.Promise = global.Promise;
+        console.log("MongoDB connection with retry");
+        mongoose
+          .connect('mongodb://localhost:27017/softuni-movies', options)
+          .then(() => {
+            console.log("MongoDB is connected");
+            app.emit("ready");
+          })
+          .catch((err) => {
+            console.log("MongoDB connection unsuccessful, retry after 2 seconds.", err);
+            setTimeout(connectWithRetry, 2000);
+          });
+      };
+
+      connectWithRetry();
+
+//   await mongoose.connect('mongodb://mongo:27017/softuni-movies', options)
+//                 .then(() => {
+//                   console.log("MongoDB is connected");
+//                   app.emit("ready");
+//                 })
+//                 .catch((err) => {
+//                   console.log("MongoDB connection unsuccessful, retry after 2 seconds.", err);
+//                   setTimeout(connectWithRetry, 2000);
+//                 });
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+//#endregion
+
 app.get('/', (req, res) => {
     res.json({ message: 'It\s working!' });
 });
