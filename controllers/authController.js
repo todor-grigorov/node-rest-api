15515a1e15dc12ec5923b01b555a18aa342fda79
todor-grigorov <t.grigorov99@gmail.com>
2023-08-30 23:31:09 +0200

    feat: init development of /register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
 
+const User = require('../models/User');
+
 router.post('/register', (req, res) => {
     console.log(req.body);
+    //TODO: Check if user exists
 
-    res.json({ ok: true });
+    let user = new User(req.body);
+
+    user.save()
+        .then(createdUser => {
+            console.log(createdUser);
+            res.status(201).json({_id: createdUser._id});
+        })
 });
 
 module.exports = router;
