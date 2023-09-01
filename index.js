0d4e411377d0336d03f79c9f1d162d1b7aad2c53
todor-grigorov <t.grigorov99@gmail.com>
2023-09-01 23:37:48 +0200

    refactor: db connection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const db = require("./db");
 
 const port = 5000;
 const app = express();
@@ -13,47 +14,8 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 //#endregion
 
-//#region Mongoose
-// mongoose.set('strictQuery', true);
-main().catch(err => console.log(err));
-
-async function main() {
-    const options = {
-        useNewUrlParser: true,
-        autoIndex: false, // Don't build indexes
-          maxPoolSize: 10, // Maintain up to 10 socket connections
-      };
-
-      const connectWithRetry = () => {
-        mongoose.Promise = global.Promise;
-        console.log("MongoDB connection with retry");
-        mongoose
-          .connect('mongodb://localhost:27017/softuni-movies', options)
-          .then(() => {
-            console.log("MongoDB is connected");
-            app.emit("ready");
-          })
-          .catch((err) => {
-            console.log("MongoDB connection unsuccessful, retry after 2 seconds.", err);
-            setTimeout(connectWithRetry, 2000);
-          });
-      };
-
-      connectWithRetry();
-
-//   await mongoose.connect('mongodb://mongo:27017/softuni-movies', options)
-//                 .then(() => {
-//                   console.log("MongoDB is connected");
-//                   app.emit("ready");
-//                 })
-//                 .catch((err) => {
-//                   console.log("MongoDB connection unsuccessful, retry after 2 seconds.", err);
-//                   setTimeout(connectWithRetry, 2000);
-//                 });
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
-//#endregion
+//connection from db here
+db.connect(app);
 
 app.get('/', (req, res) => {
     res.json({ message: 'It\s working!' });
