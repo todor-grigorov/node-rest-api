3b77ffd79d2c15a9609e9fb407090e37238ef2c0
todor-grigorov <t.grigorov99@gmail.com>
2023-09-03 22:18:18 +0200

    feat: add post  movie controler

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
-router.post('/movie', (req, res) => {
-    res.json({ ok: true });
+router.post('/', (req, res) => {
+    console.log(req.body);
+
+    let movie = new Movie(req.body);
+
+    movie.save()
+         .then(createdMovie => {
+            res.status(201).json({_id: createdMovie._id});
+         })
 });
 
 module.exports = router;
