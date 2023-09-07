39210e897fae0e7f52fc3a8241c93b32f68c651d
todor-grigorov <t.grigorov99@gmail.com>
2023-09-07 21:33:00 +0200

    refactor: enhance response returned by isAuth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,32 +1,32 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-    let authorizationHeader = req.get('Authorization');
+  let authorizationHeader = req.get("Authorization");
 
-    if (authorizationHeader) {
-        let token = authorizationHeader.split(' ')[1];
+  if (authorizationHeader) {
+    let token = authorizationHeader.split(" ")[1];
 
-        try {
-           const decoded = jwt.verify(token, 'SOMESUPERSECRET');
-           req.user = decoded;
-        } catch (error) {
-            console.error('Invalid token!');
-            return next();            
-        }
+    try {
+      const decoded = jwt.verify(token, "SOMESUPERSECRET");
+      req.user = decoded;
+    } catch (error) {
+      console.error("Invalid token!");
+      return next();
     }
+  }
 
-    next();
+  next();
 };
 
 const isAuth = (req, res, next) => {
-    if (!req.user) {
-        res.status(401).end();
-    }
+  if (!req.user) {
+    res.status(401).json({ errorData: "Invalid token!" });
+  }
 
-    next();
+  next();
 };
 
 module.exports = {
-    isAuth,
-    auth,
-}
+  isAuth,
+  auth,
+};
